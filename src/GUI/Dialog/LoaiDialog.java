6d33d7d14598ea0efa50e77459fbf726ead2b8aa
Remilia172112 <trangianguyen.com@gmail.com>
2024-11-29 16:03:28 +0700

    Thêm đơn vị, loại

---
+++ src/GUI/Dialog/LoaiDialog.java
@@ -0,0 +1,203 @@
+package GUI.Dialog;
+
+import BUS.LoaiBUS;
+import BUS.NhomQuyenBUS;
+import DAO.LoaiDAO;
+import DTO.LoaiDTO;
+import GUI.Component.ButtonCustom;
+import GUI.Component.HeaderTitle;
+import GUI.Component.InputForm;
+import GUI.Panel.ThuocTinhSanPham;
+import helper.Validation;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+public class LoaiDialog extends JDialog implements MouseListener {
+
+    ThuocTinhSanPham qlttsp;
+    HeaderTitle headTite;
+    JPanel top, main, bottom, all;
+    InputForm ms;
+    DefaultTableModel tblModel;
+    JTable table;
+    JScrollPane scrollTable;
+    ButtonCustom add, del, update;
+    LoaiBUS msBUS = new LoaiBUS();
+    ArrayList<LoaiDTO> list = msBUS.getAll();
+    private final NhomQuyenBUS nhomquyenBus = new NhomQuyenBUS();
+
+    public LoaiDialog(JFrame onwer, ThuocTinhSanPham qltt, String title, boolean modal, int nhomquyen) {
+        super(onwer, title, modal);
+        initComponent(qltt);
+        loadQuyen(nhomquyen);
+        loadDataTable(list);
+    }
+
+    public void loadQuyen(int nhomquyen) {
+        if (!nhomquyenBus.checkPermisson(nhomquyen, "thuoctinh", "create")) {
+            add.setVisible(false);
+        }
+        if (!nhomquyenBus.checkPermisson(nhomquyen, "thuoctinh", "delete")) {
+            del.setVisible(false);
+        }
+        if (!nhomquyenBus.checkPermisson(nhomquyen, "thuoctinh", "update")) {
+            update.setVisible(false);
+        }
+    }
+
+    public void initComponent(ThuocTinhSanPham qlttsp) {
+        this.qlttsp = qlttsp;
+        this.setSize(new Dimension(425, 500));
+        this.setLayout(new BorderLayout(0, 0));
+        this.setResizable(false);
+        headTite = new HeaderTitle("LOẠI SẢN PHẨM");
+        this.setBackground(Color.white);
+        top = new JPanel();
+        main = new JPanel();
+        bottom = new JPanel();
+
+        top.setLayout(new GridLayout(1, 1));
+        top.setBackground(Color.WHITE);
+        top.setPreferredSize(new Dimension(0, 70));
+        top.add(headTite);
+
+        main.setBackground(Color.WHITE);
+        main.setPreferredSize(new Dimension(420, 200));
+        ms = new InputForm("Tên loại");
+        ms.setPreferredSize(new Dimension(250, 70));
+        table = new JTable();
+        table.setBackground(Color.WHITE);
+        table.addMouseListener(this);
+        scrollTable = new JScrollPane(table);
+        scrollTable.setBackground(Color.WHITE);
+        tblModel = new DefaultTableModel();
+        String[] header = new String[]{"Mã loại", "Tên loại"};
+        tblModel.setColumnIdentifiers(header);
+        table.setModel(tblModel);
+        scrollTable.setViewportView(table);
+        scrollTable.setPreferredSize(new Dimension(420, 250));
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        TableColumnModel columnModel = table.getColumnModel();
+        columnModel.getColumn(0).setCellRenderer(centerRenderer);
+        columnModel.getColumn(1).setCellRenderer(centerRenderer);
+        main.add(ms);
+        main.add(scrollTable);
+
+        add = new ButtonCustom("Thêm", "excel", 15, 100, 40);
+        add.addMouseListener(this);
+        del = new ButtonCustom("Xóa", "danger", 15, 100, 40);
+        del.addMouseListener(this);
+        update = new ButtonCustom("Sửa", "success", 15, 100, 40);
+        update.addMouseListener(this);
+        bottom.setBackground(Color.white);
+        bottom.setLayout(new FlowLayout(1, 20, 20));
+        bottom.add(add);
+        bottom.add(del);
+        bottom.add(update);
+        bottom.setPreferredSize(new Dimension(0, 70));
+
+        this.add(top, BorderLayout.NORTH);
+        this.add(main, BorderLayout.CENTER);
+        this.add(bottom, BorderLayout.SOUTH);
+        this.setLocationRelativeTo(null);
+    }
+
+    public void loadDataTable(ArrayList<LoaiDTO> result) {
+        tblModel.setRowCount(0);
+        for (LoaiDTO ncc : result) {
+            tblModel.addRow(new Object[]{
+                ncc.getML(), ncc.getTENL()
+            });
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getSource() == add) {
+            if (Validation.isEmpty(ms.getText())) {
+                JOptionPane.showMessageDialog(this, "Vui lòng nhập tên loại mới");
+            } else {
+                String tenmau = ms.getText();
+                if (msBUS.checkDup(tenmau)) {
+                    int id = LoaiDAO.getInstance().getAutoIncrement();
+                    msBUS.add(new LoaiDTO(id, tenmau));
+                    loadDataTable(list);
+                    ms.setText("");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Loại đã tồn tại !");
+                }
+            }
+        } else if (e.getSource() == del) {
+            int index = getRowSelected();
+            if (index != -1) {
+                msBUS.delete(list.get(index), index);
+                loadDataTable(list);
+                ms.setText("");
+            }
+        } else if (e.getSource() == update) {
+            int index = getRowSelected();
+            if (index != -1) {
+                if (Validation.isEmpty(ms.getText())) {
+                    JOptionPane.showMessageDialog(this, "Vui lòng nhập tên loại mới");
+                } else {
+                    String tenmau = ms.getText();
+                    if (msBUS.checkDup(tenmau)) {
+                        msBUS.update(new LoaiDTO(list.get(index).getML(), tenmau));
+                        loadDataTable(list);
+                        ms.setText("");
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Loại đã tồn tại !");
+                    }
+                }
+            }
+        } else if (e.getSource() == table) {
+            int index = table.getSelectedRow();
+            ms.setText(list.get(index).getTENL());
+        }
+    }
+
+    public int getRowSelected() {
+        int index = table.getSelectedRow();
+        if (index == -1) {
+            JOptionPane.showMessageDialog(this, "Vui lòng chọn loại!");
+        }
+        return index;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
